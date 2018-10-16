e3351e56dd0b2da4d54bdd43f8ff8f2711f7006c
Xenos141 <arturomr2001@hotmail.com>
2018-10-16 08:08:44 -0500

    Se ha terminado el quiz :D

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,17 @@ import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { BocinaPage } from '../pages/bocina/bocina';
+import { TelePage } from '../pages/tele/tele';
+import { LamparaPage } from '../pages/lampara/lampara';
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    HomePage,
+    BocinaPage,
+    TelePage,
+    LamparaPage
   ],
   imports: [
     BrowserModule,
@@ -19,7 +25,10 @@ import { HomePage } from '../pages/home/home';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+    HomePage,
+    BocinaPage,
+    TelePage,
+    LamparaPage
   ],
   providers: [
     StatusBar,
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,29 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { BocinaPage } from '../bocina/bocina';
+import { TelePage } from '../tele/tele';
+import { LamparaPage } from '../lampara/lampara';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  bocina=BocinaPage;
+  tv=TelePage;
+  lamp=LamparaPage;
 
   constructor(public navCtrl: NavController) {
-
+    
+  }
+  bocina1(){
+    this.navCtrl.push(this.bocina)
+  }
+  tele(){
+    this.navCtrl.push(this.tv)
+  }
+  lampara(){
+    this.navCtrl.push(this.lamp)
   }
 
 }
